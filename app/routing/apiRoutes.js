bd0949a9129c3a098469ac4b151ecce069787e83
Mac User <alexarobinson19@gmail.com>
2018-01-31 21:09:47 -0500

    finished logic for choosing the book and the html for home.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,6 +7,30 @@ app.get("api/books", function(req, res){
 
 app.post("/api/books", function(req, res) {
    //need the internal structure to this once I have more information about the survey data
-  });
+var userInput = req.body;
+var totalDifference =0;
+var differentArray = [];
+var chosenBook;
+console.log('scores', userInput.scores);
+for(var i=0; i<booksData.length; i++){
+    for(var j=0; j<userInput.scores.length;j++){
+        var scoreDiff = Math.abs(booksData[i].scores[j]-userInput.scores[j]);
+        totalDifference += scoreDiff;
+    }
+    var bookObject = {
+        title:booksData[i].title,
+        photo:booksData[i].photo,
+        totalDifference:totalDifference};
+        differentArray.push(bookObject);        
+    }
+    var lowest = Number.POSITIVE_INFINITY;
+    for (var i=0; i<differentArray.length; i++) {
+        tmp = differentArray[i].totalDifference;
+        if (tmp < lowest){ 
+            lowest = tmp;
+            chosenBook = differentArray[i];
+    }};
+    res.send(chosenBook);
+});
 
 };
